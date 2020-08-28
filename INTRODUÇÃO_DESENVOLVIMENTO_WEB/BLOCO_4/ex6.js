07a4aab80264ce93b50c822f15d17f2618196782
danielmlduarte <danielduarte.tryber@gmail.com>
2020-08-27 21:32:36 -0300

    Código do 7° programa e atualização do 6°

--- INTRODUÇÃO_DESENVOLVIMENTO_WEB/BLOCO_4/ex6.js
+++ INTRODUÇÃO_DESENVOLVIMENTO_WEB/BLOCO_4/ex6.js
@@ -1,9 +1,10 @@
 const a = Math.round((Math.random()*10));
 const b = Math.round((Math.random()*10));
+console.log(' Valor de A é: ' + a + ' e o valor de B é: ' + b);
 if (a > b) {
-  console.log('A variável maior é \'A\' e seu valor é ' + a);
+  console.log('A variável maior é A');
 } else if (b > a) {
-  console.log('A variável maior é \'B\' e seu valor é ' + b);
+  console.log('A variável maior é B');
 } else {
   console.log('As duas variáveis tem o mesmo valor');
 }

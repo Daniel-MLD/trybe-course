c025f66d4e847416729ff510bb55fd4937265f59
danielmlduarte <danielduarte.tryber@gmail.com>
2020-08-28 18:02:08 -0300

    Código do exercício bônus 3 do dia 4.2

---
+++ INTRODUÇÃO/BLOCO_4/DIA_2/exBonus3.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers2 = [];
+for (let i = 0; i < numbers.length; i += 1) {
+  if (i < (numbers.length-1)) {
+    numbers2[i] = numbers[i] * numbers[i+1];
+  } else {
+    numbers2[i] = numbers[i] * 2;
+  }
+}
+console.log(numbers2);

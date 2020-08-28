15f2128ee912f8e8d3aa5be507b03b425c44fb27
danielmlduarte <danielduarte.tryber@gmail.com>
2020-08-28 00:30:29 -0300

    Código do 11° programa

---
+++ INTRODUÇÃO/BLOCO_4/DIA_1/ex11.js
@@ -0,0 +1,16 @@
+let nota = Math.round((Math.random()*100));
+console.log('Seu percentual de acertos é de: ' + nota + '%');
+if (nota >= 90) {
+  nota = 'A';
+} else if (nota >= 80) {
+  nota = 'B';
+} else if (nota >= 70) {
+  nota = 'C';
+} else if (nota >= 60) {
+  nota = 'D';
+} else if (nota >= 50) {
+  nota = 'E';
+} else {
+  nota = 'F';
+}
+console.log('Que equivale a um conceito ' + nota);

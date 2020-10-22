c35c8cacbb4ddfe3d9f87024814786dd7526653c
danielmlduarte <danielduarte.tryber@gmail.com>
2020-10-22 15:57:16 -0300

    Exercicio do dia 2 do Bloco 9

---
+++ INTRODUÇÃO/BLOCO_9/DIA_2/apiScript.js
@@ -0,0 +1,39 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  const h2 = document.getElementById('jokeContainer');
+  
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => h2.innerText = data.joke)
+    .catch();
+};
+
+let numbersResult = [2, 3, 5, 10];
+
+const myPromise = new Promise((resolve, reject) => {
+  let numbers = [];
+  for (let index = 0; index < 10; index += 1) {
+    const randomNumber = Math.round((Math.random() * 50));
+    numbers.push(Math.pow(randomNumber, 2));
+  }
+  const total = numbers.reduce((result, current) => result + current);
+  if (total >= 8000) {
+    return reject(total);
+  }
+  resolve(total);
+})
+.then(total => { 
+  numbersResult = numbersResult.map((number) => total / number)
+  console.log(numbersResult);
+  return numbersResult;
+})
+.then(thenArray => console.log(thenArray.reduce((result, current) => result + current)))
+.catch(total => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+
+window.onload = () => fetchJoke();

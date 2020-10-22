9207e9a6221124917112ae5ce86611808be7374e
danielmlduarte <danielduarte.tryber@gmail.com>
2020-10-22 19:40:53 -0300

    Refatorando exercicio do dia 2 do Bloco 9

--- INTRODUÇÃO/BLOCO_9/DIA_2/apiScript.js
+++ INTRODUÇÃO/BLOCO_9/DIA_2/apiScript.js
@@ -16,24 +16,28 @@ const fetchJoke = () => {
 
 let numbersResult = [2, 3, 5, 10];
 
-const myPromise = new Promise((resolve, reject) => {
-  let numbers = [];
-  for (let index = 0; index < 10; index += 1) {
-    const randomNumber = Math.round((Math.random() * 50));
-    numbers.push(Math.pow(randomNumber, 2));
-  }
-  const total = numbers.reduce((result, current) => result + current);
-  if (total >= 8000) {
-    return reject(total);
-  }
-  resolve(total);
-})
+const numbersWorks = () => {
+  return new Promise((resolve, reject) => {
+    let numbers = [];
+    for (let index = 0; index < 10; index += 1) {
+      const randomNumber = Math.round((Math.random() * 50));
+      numbers.push(Math.pow(randomNumber, 2));
+    }
+    const total = numbers.reduce((result, current) => result + current);
+    if (total >= 8000) {
+      return reject(total);
+    }
+    resolve(total);
+  });
+}
+
+numbersWorks()
 .then(total => { 
-  numbersResult = numbersResult.map((number) => total / number)
-  console.log(numbersResult);
-  return numbersResult;
+numbersResult = numbersResult.map((number) => total / number)
+console.log(numbersResult);
+return numbersResult;
 })
 .then(thenArray => console.log(thenArray.reduce((result, current) => result + current)))
-.catch(total => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+.catch(total => console.log('É mais de oito mil! Essa promise deve estar quebrada!' + total));
 
 window.onload = () => fetchJoke();
